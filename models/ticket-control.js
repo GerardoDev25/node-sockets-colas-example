475e70b404bfa9c278a276dea896dd00237c10c9
GerardoDev25 <GerardoDev25@gmail.com>
2021-09-01 16:34:53 -0400

    5. Modelo - Siguiente y atender nuevo ticket

--- models/ticket-control.js
+++ models/ticket-control.js
@@ -1,6 +1,13 @@
 const path = require("path");
 const fs = require("fs");
 
+class Ticket {
+   constructor(number, desktop) {
+      this.number = number;
+      this.desktop = desktop;
+   }
+}
+
 class TicketControl {
    constructor() {
       this.last = 0;
@@ -41,6 +48,36 @@ class TicketControl {
    saveDB() {
       const dbpath = path.join(__dirname, "../db/data.json");
       fs.writeFileSync(dbpath, JSON.stringify(this.toJson));
+   }
+
+   // ? function that add new ticket
+   next() {
+      this.last += 1;
+      const ticket = new Ticket(thi.last, null);
+      this.tickets.push(ticket);
+
+      this.saveDB();
+      return "Ticket: " + ticket.number;
+   }
+
+   // ? function that attend last ticket
+   attendTicket(desktop) {
+      if (!this.tickets.length) return null;
+
+      // * get last ticket and delete
+      const ticket = this.tickets.shift();
+
+      ticket.desktop = desktop;
+
+      // * add to init the ticket to array
+      this.last4.unshift(ticket);
+
+      if (this.last4.length > 4) this.last4.splice(-1, 1);
+
+      // * save to data
+      this.saveDB();
+
+      return ticket;
    }
 }
 
