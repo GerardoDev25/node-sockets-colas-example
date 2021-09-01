e7f02f305d7c701b334f6211a4ea89bf4d304e4b
GerardoDev25 <GerardoDev25@gmail.com>
2021-09-01 17:30:14 -0400

    7. Preparar pantalla de escritorio

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,34 @@
+const lblDestop = document.querySelector("h1");
+const btnNew = document.querySelector("button");
 
+const searchParams = new URLSearchParams(window.location.search);
 
-console.log('Escritorio HTML');
+if (!searchParams.has("escritorio")) {
+   window.location = "index.html";
+   throw new Error("the desktop is required");
+}
+
+const desktop = searchParams.get("escritorio");
+console.log(desktop);
+
+lblDestop.textContent = desktop;
+
+const socket = io();
+
+socket.on("connect", () => {
+   btnNew.disabled = false;
+});
+
+socket.on("disconnect", () => {
+   btnNew.disabled = true;
+});
+
+socket.on("last-ticket", (last) => {
+//    lblNuevoTicket.textContent = "Ticket: " + last;
+});
+
+btnNew.addEventListener("click", () => {
+//    socket.emit("next-ticket", null, (ticket) => {
+//       lblNuevoTicket.textContent = ticket;
+//    });
+});
